5e1dc9f6a4133ff45d10b589de6ef4f3adea82c5
Savo <savo436@gmail.com>
2025-04-09 14:55:54 +0200

    new t

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Nav from "./general/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <div className="bg-black h-[50vh]"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/general/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="absolute left-0 right-0 top-0">
+      <div className="wrapper">
+        <div className="flex w-full gap-[80px]">
+          <div className="py-[10px]">
+            <img
+              alt="logo"
+              src="./imgs/logo.png"
+              className="max-w-[210px]"
+            ></img>
+          </div>
+          <div className="flex-1 flex-col">
+            <div className="bg-white flex min-h-[50px]">dsadsa</div>
+            <div>dsa</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
